4d4dfc792139276f4397a0e3f2cb99ac155acd8b
Igor <lishchuk.is@i.ua>
2018-03-10 17:53:09 +0200

    first commit

---
+++ Approximation/Approximation/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Апроксимация
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dot1 = new System.Windows.Forms.Button();
+            this.Dot2 = new System.Windows.Forms.Button();
+            this.Dot3 = new System.Windows.Forms.Button();
+            this.Dot4 = new System.Windows.Forms.Button();
+            this.Dot5 = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BackImageTransparentColor = System.Drawing.Color.Transparent;
+            this.chart1.BackSecondaryColor = System.Drawing.Color.Black;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.BorderColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderSkin.PageColor = System.Drawing.Color.Transparent;
+            chartArea1.AlignmentOrientation = ((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations)((System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Vertical | System.Windows.Forms.DataVisualization.Charting.AreaAlignmentOrientations.Horizontal)));
+            chartArea1.AxisX.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisX.Crossing = 0D;
+            chartArea1.AxisX.Interval = 1D;
+            chartArea1.AxisX.IntervalOffset = 1D;
+            chartArea1.AxisX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.IsMarginVisible = false;
+            chartArea1.AxisX.LabelStyle.Angle = 1;
+            chartArea1.AxisX.LabelStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            chartArea1.AxisX.LabelStyle.IsEndLabelVisible = false;
+            chartArea1.AxisX.MajorGrid.Interval = 0D;
+            chartArea1.AxisX.MajorGrid.IntervalOffset = 1D;
+            chartArea1.AxisX.MajorGrid.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea1.AxisX.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea1.AxisX.MajorTickMark.TickMarkStyle = System.Windows.Forms.DataVisualization.Charting.TickMarkStyle.None;
+            chartArea1.AxisX.Maximum = 6D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.ScaleBreakStyle.MaxNumberOfBreaks = 1;
+            chartArea1.AxisX.ScaleBreakStyle.Spacing = 1D;
+            chartArea1.AxisX.ScaleBreakStyle.StartFromZero = System.Windows.Forms.DataVisualization.Charting.StartFromZero.No;
+            chartArea1.AxisX.Title = "x";
+            chartArea1.AxisX.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea1.AxisX.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            chartArea1.AxisX2.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisX2.InterlacedColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            chartArea1.AxisX2.LineColor = System.Drawing.Color.Maroon;
+            chartArea1.AxisX2.MajorGrid.Interval = 0D;
+            chartArea1.AxisX2.MajorGrid.IntervalOffset = 0D;
+            chartArea1.AxisX2.MajorGrid.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea1.AxisX2.MajorGrid.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Auto;
+            chartArea1.AxisX2.MajorGrid.LineColor = System.Drawing.Color.DarkRed;
+            chartArea1.AxisX2.MajorGrid.LineWidth = 5;
+            chartArea1.AxisX2.MajorTickMark.LineColor = System.Drawing.Color.Maroon;
+            chartArea1.AxisY.ArrowStyle = System.Windows.Forms.DataVisualization.Charting.AxisArrowStyle.Triangle;
+            chartArea1.AxisY.Crossing = 0D;
+            chartArea1.AxisY.Interval = 1D;
+            chartArea1.AxisY.IntervalOffset = 1D;
+            chartArea1.AxisY.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.IsMarginVisible = false;
+            chartArea1.AxisY.Maximum = 10D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.ScrollBar.BackColor = System.Drawing.Color.Red;
+            chartArea1.AxisY.ScrollBar.ButtonColor = System.Drawing.Color.Purple;
+            chartArea1.AxisY.ScrollBar.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            chartArea1.AxisY.TextOrientation = System.Windows.Forms.DataVisualization.Charting.TextOrientation.Horizontal;
+            chartArea1.AxisY.Title = "y";
+            chartArea1.AxisY.TitleAlignment = System.Drawing.StringAlignment.Far;
+            chartArea1.AxisY.TitleFont = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold);
+            chartArea1.InnerPlotPosition.Auto = false;
+            chartArea1.InnerPlotPosition.Height = 90F;
+            chartArea1.InnerPlotPosition.Width = 90F;
+            chartArea1.InnerPlotPosition.X = 5F;
+            chartArea1.InnerPlotPosition.Y = 3F;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.Position.Auto = false;
+            chartArea1.Position.Height = 90F;
+            chartArea1.Position.Width = 90F;
+            chartArea1.Position.X = 5F;
+            chartArea1.Position.Y = 3F;
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
+            legend1.ItemColumnSeparator = System.Windows.Forms.DataVisualization.Charting.LegendSeparatorStyle.DotLine;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series1.BackImageTransparentColor = System.Drawing.Color.White;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Maroon;
+            series1.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.NotSet;
+            series1.Legend = "Legend1";
+            series1.MarkerBorderColor = System.Drawing.Color.Black;
+            series1.MarkerSize = 2;
+            series1.Name = "MNK";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Lagrangian";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(740, 617);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.label1.Location = new System.Drawing.Point(576, 599);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Dot1
+            // 
+            this.Dot1.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.Dot1.Location = new System.Drawing.Point(165, 480);
+            this.Dot1.Name = "Dot1";
+            this.Dot1.Size = new System.Drawing.Size(10, 10);
+            this.Dot1.TabIndex = 2;
+            this.Dot1.UseVisualStyleBackColor = true;
+            this.Dot1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.Dot1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dot1_MouseMove);
+            this.Dot1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // Dot2
+            // 
+            this.Dot2.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.Dot2.Location = new System.Drawing.Point(265, 480);
+            this.Dot2.Name = "Dot2";
+            this.Dot2.Size = new System.Drawing.Size(10, 10);
+            this.Dot2.TabIndex = 2;
+            this.Dot2.UseVisualStyleBackColor = true;
+            this.Dot2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.Dot2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dot2_MouseMove);
+            this.Dot2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // Dot3
+            // 
+            this.Dot3.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.Dot3.Location = new System.Drawing.Point(365, 480);
+            this.Dot3.Name = "Dot3";
+            this.Dot3.Size = new System.Drawing.Size(10, 10);
+            this.Dot3.TabIndex = 2;
+            this.Dot3.UseVisualStyleBackColor = true;
+            this.Dot3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            this.Dot3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dot3_MouseMove);
+            this.Dot3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button3_MouseUp);
+            // 
+            // Dot4
+            // 
+            this.Dot4.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.Dot4.Location = new System.Drawing.Point(465, 480);
+            this.Dot4.Name = "Dot4";
+            this.Dot4.Size = new System.Drawing.Size(10, 10);
+            this.Dot4.TabIndex = 2;
+            this.Dot4.UseVisualStyleBackColor = true;
+            this.Dot4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            this.Dot4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dot4_MouseMove);
+            this.Dot4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // Dot5
+            // 
+            this.Dot5.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.Dot5.Location = new System.Drawing.Point(565, 480);
+            this.Dot5.Name = "Dot5";
+            this.Dot5.Size = new System.Drawing.Size(10, 10);
+            this.Dot5.TabIndex = 2;
+            this.Dot5.UseVisualStyleBackColor = true;
+            this.Dot5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button5_MouseDown);
+            this.Dot5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Dot5_MouseMove);
+            this.Dot5.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button5_MouseUp);
+            // 
+            // Start
+            // 
+            this.Start.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Start.Location = new System.Drawing.Point(317, 558);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(213, 54);
+            this.Start.TabIndex = 3;
+            this.Start.Text = "Approximation!";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(747, 624);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Dot5);
+            this.Controls.Add(this.Dot4);
+            this.Controls.Add(this.Dot3);
+            this.Controls.Add(this.Dot2);
+            this.Controls.Add(this.Dot1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Approximation of the function";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Dot1;
+        private System.Windows.Forms.Button Dot2;
+        private System.Windows.Forms.Button Dot3;
+        private System.Windows.Forms.Button Dot4;
+        private System.Windows.Forms.Button Dot5;
+        private System.Windows.Forms.Button Start;
+
+    }
+}
+
---
+++ Approximation/Approximation/Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Апроксимация
+{
+    public partial class Form1 : Form
+    {
+        static double[] x = new double[7]; //Массив значений икса (х)
+        static double[] y = new double[7]; //Массив значений игрика (у)
+        double[] root = new double[4]; //Массив корней функции при аппроксимации МНК
+        int coordinate_y; //Значение игрик (у) для корректного перемещения кнопки по форме
+        bool IsCliked; //Проверка нажата ли кнопка мышки
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 7; i++) //Задаём начальные значения иксов (х) и игриков (у) при загрузке формы и линию, на которой лежат начальные точки
+            {
+                x[i] = i;
+                y[i] = 1;
+                chart1.Series[0].Points.AddXY(x[i], y[i]);
+            }
+        }
+
+
+        
+
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            double[] summ = new double[7];
+            double[] total = new double[4];
+            double[,] a = new double[5, 5];
+
+            summ[0] = 5; //Сумма колличества иксов (х)
+            a[0, 4] = totals(1, 5, 2); //Вызов метода totals с k=2
+
+            for (int i = 0; i < 6; i++)
+            {
+                summ[i + 1] = totals(i, 5, 0); //Вызов метода totals с k=0
+
+                if (i < 3)
+                    a[i + 1, 4] = totals(i, 5, 1); //Вызов метода totals с k=1
+            }
+
+            for (int q = 0; q < 4; q++)
+            {
+                for (int w = 0; w < 4; w++)
+                {
+                    a[q, w] = summ[w + q]; //Создание системы уравнений для полинома третьей степени
+                } 
+            }
+
+            for (int i_ = 0; i_ < 4; i_++) //Упрощение матрицы методом Гаусса
+                for (int i = i_ + 1; i < 4; i++)
+                {
+                    double k = -a[i, i_] / a[i_, i_]; //Коефициент для упрощения первых элементов системы каждой строчки (кроме строчки с индексом i_)
+                    for (int q = i_ + 1; q < 5; q++)
+                    {
+                        a[i, q] = a[i, q] + a[i_, q] * k; //Умножение каждого элемента каждой строчки (кроме строчки с индексом i_) на найденный коефициент (k)
+                    }
+                }
+
+            for (int i = 3; i > -1; i--)
+            {
+                double summ_ = a[i, 4]; //Записываем в переменную summ_ правую часть уравнения (одно число)
+                int k_ = 3; //Переменная индексов элементов левой части уравнения
+                for (int i_ = 3 - i; i_ > 0; i_--)
+                {
+                    summ_ -= a[i, k_] * root[k_]; //Отнимаем от правой части элементы, которые в левой части
+                    k_--;
+                }
+                root[i] = summ_ / a[i, i]; //Находим корень i-той строчки
+            }
+
+
+            for (int i = 0; i < 7; i++) //Рисуем график (аппроксимированный МНК)
+            {
+                chart1.Series[0].Points.AddXY(x[i], MNK(x[i]));
+            }
+
+            for (int i = 0; i < 7; i++) //Рисуем график (интерполяция за Лагранжем)
+            {
+                chart1.Series[1].Points.AddXY(x[i], Lagrange(x[i]));
+            }
+        }
+
+
+        double totals(int power, int i, int k) //Рекурсия. power -- нужная степень, i -- колличество повторений, k -- номер нужного действия (в switch..case)
+        {
+            double result = 0;
+            if (i != 0)
+            {
+                switch (k)
+                {
+                    case 0:
+                        { result = Math.Pow(x[i], power + 1) + totals(power, i - 1, k); break; } //Сумма иксов (x) в степени power+1
+                    case 1:
+                        { result = Math.Pow(x[i], power + 1) * y[i] + totals(power, i - 1, k); break; } //Сумма умножений игрика (у) на икса (х) в степени power+1
+                    case 2:
+                        { result = y[i] + totals(power, i - 1, k); break; } //Сумма игриков (у) в первой степени (вызывается лишь строчкой "a[0, 4] = totals(1, 4, 2);")
+                }
+            }
+            return result;
+        }
+
+
+
+        double MNK(double f_x) //Метод функции игрик (у) от икса (х)
+        {
+            f_x = root[3] * Math.Pow(f_x, 3) + root[2] * Math.Pow(f_x, 2) + root[1] * f_x + root[0];
+            return f_x;
+        }
+
+        double Lagrange(double f_x) //Метод возвращает значение функции от икса (х) (интерполяция за Лагранжем)
+        {
+            double Answer = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                double L = y[i];
+                for (int i_ = 0; i_ < 7; i_++)
+                {
+                    if (i != i_)
+                        L *= (f_x - x[i_]) / (x[i] - x[i_]);
+                }
+                Answer += L;
+            }
+            return Answer;
+        }
+
+
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                double yValue = chart1.ChartAreas[0].AxisY.PixelPositionToValue(e.Y);
+                double xValue = chart1.ChartAreas[0].AxisX.PixelPositionToValue(e.X);
+                if (xValue > 0 && xValue < 6 && yValue > 0 && yValue < 10) //Границы диаграммы
+                {
+                    chart1.Cursor = System.Windows.Forms.Cursors.Cross;
+                    label1.Visible = true;
+                    label1.Text = string.Concat(String.Concat(Math.Round(xValue, 1).ToString(), " , "), Math.Round(yValue, 1).ToString()); //Показывает координаты икса (х) и игрика (у)
+                    label1.Location = new Point(e.X + 5, e.Y); //Расположение формы со значениями координат
+                }
+                else //Курсор не в границах диаграммы
+                {
+                    label1.Visible = false;
+                    chart1.Cursor = System.Windows.Forms.Cursors.Default;
+                }
+            }
+            catch { }
+            finally { }
+           
+        }
+
+        bool borders(int coordinate) //Возвращает выходит ли точка за границы диаграммы
+        {
+            if (coordinate > 30 && coordinate < 530) //Границы диаграммы для точек
+                return true;
+            else 
+                return false;
+        }
+
+
+        ////////////////////////////////////////////////////////////////
+        //Нажатие кнопкой мышки по точке
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsCliked = true; //Кнопка мышки нажата
+            coordinate_y = Dot1.Location.Y - Cursor.Position.Y; //Переменная используемая для корректного рассположение точки после нажатия на неё
+        }
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsCliked = true;
+            coordinate_y = Dot2.Location.Y - Cursor.Position.Y;
+        }
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsCliked = true;
+            coordinate_y = Dot3.Location.Y - Cursor.Position.Y;
+        }
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsCliked = true;
+            coordinate_y = Dot4.Location.Y - Cursor.Position.Y;
+        }
+        private void button5_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsCliked = true;
+            coordinate_y = Dot5.Location.Y - Cursor.Position.Y;
+        }
+        ////////////////////////////////////////////////////////////////
+
+
+
+        ////////////////////////////////////////////////////////////////
+        //Отпускание кнопки мыши
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsCliked = false; //Кнопки мыши отжата
+        }
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsCliked = false;
+        }
+        private void button3_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsCliked = false;
+        }
+        private void button4_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsCliked = false;
+        }
+        private void button5_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsCliked = false;
+        }
+        ////////////////////////////////////////////////////////////////
+
+
+
+        ////////////////////////////////////////////////////////////////
+        //Движение точки по диаграмме
+        private void Dot1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsCliked) //Если кнопка мыши нажата
+            {
+                if (borders(coordinate_y + Cursor.Position.Y)) //Проверка не выходит ли точка за границы
+                {
+                    Dot1.Top = coordinate_y + Cursor.Position.Y; //Перемещение точки по диаграмме
+                    Label_coordinate(Dot1.Left, Dot1.Top, 1); //Вызов метода для вывода формы с координатами точки, принимает координаты точки
+                }
+            }
+        }
+        private void Dot2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsCliked)
+            {
+                if (borders(coordinate_y + Cursor.Position.Y))
+                {
+                    Dot2.Top = coordinate_y + Cursor.Position.Y;
+                    Label_coordinate(Dot2.Left, Dot2.Top, 2);
+                }
+            }
+        }
+        private void Dot3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsCliked)
+            {
+                if (borders(coordinate_y + Cursor.Position.Y))
+                {
+                    Dot3.Top = coordinate_y + Cursor.Position.Y;
+                    Label_coordinate(Dot3.Left, Dot3.Top, 3);
+                }
+            }
+        }
+        private void Dot4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsCliked)
+            {
+                if (borders(coordinate_y + Cursor.Position.Y))
+                {
+                    Dot4.Top = coordinate_y + Cursor.Position.Y;
+                    Label_coordinate(Dot4.Left, Dot4.Top, 4);
+                }
+            }
+        }
+        private void Dot5_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsCliked)
+            {
+                if (borders(coordinate_y + Cursor.Position.Y))
+                {
+                    Dot5.Top = coordinate_y + Cursor.Position.Y;
+                    Label_coordinate(Dot5.Left, Dot5.Top, 5);
+                }
+            }
+        }
+        ////////////////////////////////////////////////////////////////
+
+
+        void Label_coordinate(int but_left, int butt_top, int i) //Меняет расположение формы с координатами точки
+        {
+            label1.Location = new Point(but_left + 12, butt_top); //Меняет расположение формы относительно расположение точки
+            //value_y(butt_top - 5, i); //
+            y[i] = (530 - Convert.ToDouble(butt_top)) / 50; //Вычисление значение точки по игрику (у)
+            //530 -- длина диаграммы, coordinate_butt -- расположение точки на диаграмме (по длине), 50 -- расстояние между у(n) и у(n+1)
+            label1.Text = string.Concat(x[i] + ", ", Math.Round(y[i] - 0.1, 1)); //Вывод на форму значения координат точки
+        }
+
+        void value_y(double coordinate_butt, int i) //Метод вычисление значения точки по игрику (у)
+        {
+            y[i] = (530 - coordinate_butt) / 50;
+            //530 -- длина диаграммы, coordinate_butt -- расположение точки на диаграмме (по длине), 50 -- расстояние между у(n) и у(n+1)
+        }
+    }
+}
